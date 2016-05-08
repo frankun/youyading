b99f289a87f70bf0c24905e598e010494326af03
freekun <836783712@qq.com>
2016-05-08 16:15:43 +0800

    youyading

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# node-test
+# youyading
 node
---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var testJson = require('./test.json');
+app.use(express.static('public'));
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+
+app.get('/api/test',function(req,res){
+    res.json(200,testJson);
+});
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
